dc6d949e71070a3fc28830fe6a091c9e77532a30
OmidNaghi <43643964+OmidNaghi@users.noreply.github.com>
2018-10-03 13:59:19 +0200

    Add project files.

---
+++ WPF-SecurityUI/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_SecurityUI
+{
+    public class Class1
+    {
+    }
+}
